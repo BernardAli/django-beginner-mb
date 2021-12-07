68c1ba053af28b6299520d1217285021169135f5
BernardAli <alibernard.coding@gmail.com>
2021-12-07 09:08:33 +0000

    commit

---
+++ posts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+# Models
+from .models import Post
+
+
+# Create your views here.
+
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'index.html'
+    context_object_name = 'all_posts_list'
